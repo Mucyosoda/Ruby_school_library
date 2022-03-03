aa85f1e86ea2e2d886f5a8ecc437a5c88576c936
Mucyosoda <mucyosofa@gmail.com>
2022-03-03 11:58:22 +0100

    setup association

--- person.rb
+++ person.rb
@@ -7,12 +7,18 @@ class Person
     @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
+    @rentals = []
   end
 
   def can_use_services?
     return true if is_of_age or @parent_permission
 
     false
+  end
+
+  def add_rental(rental)
+    @rentals.push(rental) unless @rentals.include?(rental)
+    rental.owner = self
   end
 
   private
